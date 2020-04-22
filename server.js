2d50616acb3d908ba83e2052f7e0af94542091a7
Erol Yurtkuran <erol@yurtkuran.net>
2020-04-22 09:09:31 -0700

    clean-up items

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // bring in local modules
 const connectDB = require('./config/db');
@@ -16,12 +17,21 @@ const app = express();
 app.use(express.json({ extended: false }));
 
 // inital route
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
+// app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
 
 // define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// server static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    // set default 'route'
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', index.html)));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
